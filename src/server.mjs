b6a1f2fc28e65efe37f4c3a21b451afe9b25b7d0
gabrielle <catarinogabrielle@gmail.com>
2022-09-16 12:29:03 -0300

    first commit

---
+++ src/server.mjs
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express()
+
+// www.testeapp.com/ads  
+
+app.get('/ads', (request, response) => {
+    return response.send('acessou o ads')
+})
+
+app.listen(3333)
